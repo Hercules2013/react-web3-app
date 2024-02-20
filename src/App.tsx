c4c92a15856b2e19720b9a1d8441be85bebfb0cf
Hercules2013 <heracules2347@gmail.com>
2024-02-20 06:35:04 -0800

    feat: revert

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,35 @@
-import { Route, Routes, Link, useLocation } from 'react-router-dom'
+import { Route, Routes, Link, useLocation } from 'react-router-dom';
 
-import SwapPage from './pages/Swap'
-import PortfolioPage from './pages/Portfolio'
+import SwapPage from './pages/Swap';
+import PortfolioPage from './pages/Portfolio';
 
-import styles from './App.module.css'
+import styles from './App.module.css';
 
-import { createTheme, ThemeProvider } from '@mui/material'
-import { Layout, Menu } from 'antd'
-import { SwapOutlined, OrderedListOutlined } from '@ant-design/icons'
-const { Header } = Layout
-
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#ff0000',
-      contrastText: '#ffffff',
-    },
-    secondary: {
-      main: '#9e9e9e',
-      contrastText: '#ffffff',
-    },
-  },
-})
+import { Layout, Menu } from 'antd';
+import { SwapOutlined, OrderedListOutlined } from '@ant-design/icons';
+const { Header } = Layout;
 
 export default function App() {
-  const location = useLocation()
+  const location = useLocation();
 
   return (
-    <ThemeProvider theme={theme}>
+    <main className={styles.main}>
+      <Header className={styles.header}>
+        <Menu mode="horizontal" selectedKeys={[location.pathname]}>
+          <Menu.Item icon={<SwapOutlined rev="twoToneColor" />} key="/">
+            <Link to="/">Swap</Link>
+          </Menu.Item>
+          <Menu.Item icon={<OrderedListOutlined rev="twoToneColor" />} key="/portfolio">
+            <Link to="/portfolio">Transaction</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
       <main className={styles.main}>
-        <Header className={styles.header}>
-          <Menu mode="horizontal" selectedKeys={[location.pathname]}>
-            <Menu.Item icon={<SwapOutlined rev="twoToneColor" />} key="/">
-              <Link to="/">Swap</Link>
-            </Menu.Item>
-            <Menu.Item icon={<OrderedListOutlined rev="twoToneColor" />} key="/transaction">
-              <Link to="/transaction">Transaction</Link>
-            </Menu.Item>
-          </Menu>
-        </Header>
-        <main className={styles.main}>
-          <Routes>
-            <Route path="/" element={<SwapPage />} />
-            <Route path="/transaction" element={<PortfolioPage />} />
-          </Routes>
-        </main>
+        <Routes>
+          <Route path="/" element={<SwapPage />} />
+          <Route path="/portfolio" element={<PortfolioPage />} />
+        </Routes>
       </main>
-    </ThemeProvider>
+    </main>
   )
 }
--- src/pages/Swap.jsx
+++ src/pages/Swap.jsx
@@ -1,16 +1,29 @@
-import { SnackbarProvider } from "notistack";
+import { SwapWidget } from '@uniswap/widgets';
 
-import Web3Provider from "../network";
-import CoinSwapper from '../CoinSwapper/CoinSwapper';
+import Web3Connectors from '../components/Web3Connectors';
+import { useActiveProvider } from '../components/connectors';
+
+import { JSON_RPC_URL, TOKEN_LIST, UNI } from '../utils/constants';
+
+import styles from '../styles/swap.module.css';
 
 const Swap = () => {
+  // The provider to pass to the SwapWidget.
+  // This is a Web3Provider (from @ethersproject) supplied by @web3-react; see ./connectors.ts.
+  const provider = useActiveProvider()
+
   return (
-    <SnackbarProvider maxSnack={3}>
-      <Web3Provider render={(network) => (
-        <CoinSwapper network={network} />
-      )}>
-      </Web3Provider>
-    </SnackbarProvider>
+    <div className={styles.swapPage}>
+      <Web3Connectors />
+      <SwapWidget
+        jsonRpcEndpoint={JSON_RPC_URL}
+        tokenList={TOKEN_LIST}
+        provider={provider}
+        defaultInputTokenAddress="NATIVE"
+        defaultInputAmount="1"
+        defaultOutputTokenAddress={UNI}
+      />
+    </div>
   )
 }
 
