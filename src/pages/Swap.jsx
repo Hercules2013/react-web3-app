300f8cdf0ef54068284362757235d3c3f7721142
Hercules2013 <heracules2347@gmail.com>
2024-02-20 11:59:56 -0800

    fix: responsive of swap page

--- src/pages/Swap.jsx
+++ src/pages/Swap.jsx
@@ -1,4 +1,5 @@
 import { SwapWidget } from '@uniswap/widgets';
+import { useMediaQuery } from '@mui/material';
 
 import Web3Connectors from '../components/Web3Connectors';
 import { useActiveProvider } from '../components/connectors';
@@ -12,8 +13,10 @@ const Swap = () => {
   // This is a Web3Provider (from @ethersproject) supplied by @web3-react; see ./connectors.ts.
   const provider = useActiveProvider()
 
+  const match1 = useMediaQuery('(min-width:840px)');
+
   return (
-    <div className={styles.swapPage}>
+    <div className={styles.swapPage} style={ match1 ? { flexDirection: 'row' } : { flexDirection: 'column' }}>
       <Web3Connectors />
       <SwapWidget
         jsonRpcEndpoint={JSON_RPC_URL}
